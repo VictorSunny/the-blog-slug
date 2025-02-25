4b6ffaf40f41812dbe45450130b26da3fecbb0e5
Victor Sunny <90870360+VictorSunny@users.noreply.github.com>
2025-02-25 20:50:30 +0100

    Update views.py

--- trendyblogs/views.py
+++ trendyblogs/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
-def handler404(request, exceception):
+def handler404(request, exception):
     # return HttpResponse("oops")
     return render(request, "404.html")
 
-def handler500(request):
+def handler500(request, exception):
     return render(request, "404.html")
 
